cb5252e6724133d4b50f75e8b82db37ba1afe004
Marvin Brückner <marvin.brueckner@gmx.net>
2020-02-18 21:40:36 +0100

    First Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,112 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(FarmingMap());
+void main() => runApp(MyApp());
 
-class FarmingMap extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text(
-          'FarmingMap',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.w600
-          ),
-        ),
-      ),
+    return MaterialApp(
+      title: 'FarmingMap',
+      home: MainScreen(),
     );
   }
 }
 
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kLake = CameraPosition(
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    tilt: 59.440717697143555,
+    zoom: 19.134545
+  );
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget> [
+    Text(
+      'Index 0: Markierungen',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Map',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: ToDo',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Optionen',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FarmingMap'),
+        backgroundColor: Colors.green,
+      ),
+      body: GoogleMap(
+        mapType: MapType.terrain,
+        initialCameraPosition: CameraPosition(target: LatLng(40.688841, -74.044015), zoom: 12),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _addMark,
+        backgroundColor: Colors.green,
+        splashColor: Colors.lightGreenAccent,
+        label: Text('Neue Markierung'),
+        icon: Icon(Icons.room),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.room),
+            title: Text('Markierungen')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            title: Text('Map')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            title: Text('ToDo')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Optionen')
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Future<void> _addMark() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
